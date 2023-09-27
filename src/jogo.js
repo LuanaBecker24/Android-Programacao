29b74672ebdcd1c9ed5502131bbf2c7dd72611a3
LuanaBecker24 <130504315+LuanaBecker24@users.noreply.github.com>
2023-09-26 21:48:27 -0300

    testando

--- src/jogo.js
+++ src/jogo.js
@@ -1,14 +1,101 @@
-import { View, Text, Button } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
 
 export default function Jogo(props) {
-    const handleClick = () => {
+    const [b1, setB1] = useState("")
+    const [b2, setB2] = useState("")
+    const [b3, setB3] = useState("")
+    const [b4, setB4] = useState("")
+    const [b5, setB5] = useState("")
+    const [b6, setB6] = useState("")
+    const [b7, setB7] = useState("")
+    const [b8, setB8] = useState("")
+    const [b9, setB9] = useState("")
+
+    const handleClickB1 = (b, setb) => {
+        if(b1 === "X") {
+            setB1("O");
+        } else {setB1("X")}
+    }
+
+    const handleClickB2 = () => {
+        if(b2 === "X") {
+            setB2("O");
+        } else {setB2("X")}
+    }
+
+    const handleClickB3 = () => {
+        if(b3 === "X") {
+           setB3("O");
+        } else {setB3("X")}
+    }
+
+    const handleClickB4 = () => {
+        if(b4 === "X") {
+            setB4("O");
+        } else {setB4("X")}
+    }
+
+    const handleClickB5 = () => {
+        if(b5 === "X") {
+            setB5("O");
+        } else {setB5("X")}
+    }
+
+    const handleClickB6 = () => {
+        if(b6 === "X") {
+            setB6("O");
+        } else {setB6("X")}
+    }
+
+    const handleClickB7 = () => {
+        if(b7 === "X") {
+            setB7("O");
+        } else {setB7("X")}
+    }
+
+    const handleClickB8 = () => {
+        if(b8 === "X") {
+            setB8("O");
+        } else {setB8("X")}
+    }
+
+    const handleClickB9 = () => {
+        if(b9 === "X") {
+            setB9("O");
+        } else {setB9("X")}
+    }
+
+    const handleClick = (event) => {
         props.changeScreen("home")
     }
 
     return (
-        <View>
-            <Text>Jogo</Text>
+        <View style={styles.container}>
             <Button title="Voltar" onPress={handleClick}/>
+            <Text style={styles.text}>Jogo</Text>
+            <Button title={b1} onPress={handleClickB1}/>
+            <Button title={b2} onPress={handleClickB2}/>
+            <Button title={b3} onPress={handleClickB3}/>
+            <Button title={b4} onPress={handleClickB4}/>
+            <Button title={b5} onPress={handleClickB5}/>
+            <Button title={b6} onPress={handleClickB6}/>
+            <Button title={b7} onPress={handleClickB7}/>
+            <Button title={b8} onPress={handleClickB8}/>
+            <Button title={b9} onPress={handleClickB9}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#98c1d9',
+      alignItems: 'center',
+      justifyContent: 'center',
+      gap: 15
+    },
+    text: {
+      fontSize: 30,
+    }
+})
